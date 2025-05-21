bd67a6d1ede640d776c400da4f6d6ed88f500b66
sahil soni <sonisahil9905@gmail.com>
2025-05-21 21:11:31 +0530

    sdfdsf

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: "*",
-    credentials: true, // This will cause a conflict with `origin: "*"` — see note below
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
