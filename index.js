a67790ff381b3ea53d2ebd76f756f225035d4966
sahil soni <sonisahil9905@gmail.com>
2025-05-21 20:52:15 +0530

    sahilsoni

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow requests from Vite dev server
+    origin: "https://expense-tracker-olive-phi.vercel.app/", // Allow requests from Vite dev server
     credentials: true,
   })
 );
